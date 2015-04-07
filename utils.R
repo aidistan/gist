f79146a1171c7664ab160be94eff9805360399aa
Aidi Stan <aidistan@users.noreply.github.com>
2015-04-07 12:37:04 +0800

    Update and rename R.md to utils.R

--- utils.R
+++ utils.R
@@ -1,8 +1,6 @@
-```R
 # Install packages if not installed and require them all
 use.packages <- function(list) {
   new.packages <- list[!(list %in% installed.packages()[,'Package'])]
   if(length(new.packages)) install.packages(new.packages)
   for(pkg in list ) library(pkg, character.only = TRUE)
 }
-```

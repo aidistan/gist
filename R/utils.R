dc2b2030666a63446d77f05d15515a872abf628a
Aidi Stan <aidistan@live.com>
2015-04-29 21:33:42 +0800

    Update R/utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,23 @@
 # Install packages if not installed and require them in one line
 use.packages <- function(packages) {
-  new.packages <- packages[!(packages %in% installed.packages()[,'Package'])]
-  if(length(new.packages)) install.packages(new.packages)
+  # Find uninstalled packages
+  uninstalled <- packages[!(packages %in% installed.packages()[,'Package'])]
+
+  # Try to install all uninstalled packages
+  if(length(uninstalled)) {
+    # Install from CRAN
+    cran <- uninstalled[uninstalled %in% available.packages()[, 'Package']]
+    if(length(cran)) install.packages(cran)
+
+    # Install from BioConductor
+    bioc <- uninstalled[!(uninstalled %in% available.packages()[, 'Package'])]
+    if(length(bioc)) {
+      source("http://bioconductor.org/biocLite.R")
+      biocLite()
+      biocLite(bioc)
+    }
+  }
+
+  # Load all packages
   for(pkg in packages) library(pkg, character.only = TRUE)
 }

a44b355e7eee0aaa113dd89befb8c07c35fb2259
Aidi Stan <aidistan@users.noreply.github.com>
2016-04-10 13:32:19 +0800

    Remove unused files

--- datapaq.rb
+++
@@ -1,4 +0,0 @@
-require 'net/telnet'
-conn = Net::Telnet.new('Host' => '192.168.1.107', 'Port' => '8899', 'Timeout' => 20, "Prompt" => /^OK$/)
-conn.cmd("VERSION") { |c| print c }
-conn.cmd("SCAN 1 text") { |c| print c }

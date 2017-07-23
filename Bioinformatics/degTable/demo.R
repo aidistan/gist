9f369209b0c0bc5d56fcebdcf4dcc725696ddabf
Aidi Stan <aidistan@live.com>
2017-07-23 13:59:54 +0800

    Clean the repo

--- Bioinformatics/degTable/demo.R
+++
@@ -1,9 +0,0 @@
-source('http://aidistan.github.io/gist/R/use.packages.R')
-source('http://aidistan.github.io/gist/R/degTable.R')
-use.packages(c('GEOquery'))
-
-write.table(degTable(
-  getGEO('GSE2669', destdir='.', GSEMatrix=TRUE, AnnotGPL=TRUE)[[1]],
-  label = read.table('GSE2669.tab', header = T, row.names = 1, sep = "\t")[colnames(gset), 2],
-  contrasts = c('I-CG')
-), file='result.txt', row.names=FALSE, quote=FALSE, sep="\t")
